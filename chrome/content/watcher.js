13420df21310779455e4b476d334021895caebad
Wladimir Palant <trev@adblockplus.org>
2012-09-22 16:36:54 +0200

    Removed backwards compatibility code for ABP 2.0.x and Firefox 13

--- chrome/content/watcher.js
+++ chrome/content/watcher.js
@@ -16,10 +16,7 @@ function abprequire(module)
   let result = {};
   result.wrappedJSObject = result;
   Services.obs.notifyObservers(result, "adblockplus-require", module);
-  if ("exports" in result)
-    return result.exports;
-  else
-    return Cu.import("chrome://adblockplus-modules/content/" + module[0].toUpperCase() + module.substr(1) + ".jsm", null);
+  return result.exports;
 }
 
 let {Policy} = abprequire("contentPolicy");
@@ -28,18 +25,8 @@ let {Filter} = abprequire("filterClasses");
 
 let policyGlobal = Cu.getGlobalForObject(Policy);
 let PolicyPrivate = null;
-if (policyGlobal == window)
-{
-  // Work-around for bug 736316 - getGlobalForObject gave us our own window
-  let {XPIProvider} = Cu.import("resource://gre/modules/XPIProvider.jsm", null);
-  let addonID = "{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}"
-  if (addonID in XPIProvider.bootstrapScopes)
-    policyGlobal = XPIProvider.bootstrapScopes[addonID];
-}
 
-if ("PolicyPrivate" in policyGlobal)              // ABP 2.0.x
-  PolicyPrivate = policyGlobal.PolicyPrivate;
-else if ("PolicyImplementation" in policyGlobal)  // ABP 2.1+ with scope separation
+if ("PolicyImplementation" in policyGlobal)  // ABP 2.1+ with scope separation
   PolicyPrivate = policyGlobal.PolicyImplementation;
 else if ("require" in policyGlobal)               // ABP 2.1+ without scope separation
   PolicyPrivate = policyGlobal.require.scopes.contentPolicy.PolicyImplementation;
